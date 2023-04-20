6bb773e3569b6af022fa6fa8917bd688409f8cfc
AnujSingh718 <103810977+AnujSingh718@users.noreply.github.com>
2023-04-20 13:21:05 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <div className='footer'>
+        <p>© 2023 Airbnb clone</p>
+        <p>Privacy · Terms · Sitemap · Company Details</p>
+    </div>
+  )
+}
+
+export default Footer
